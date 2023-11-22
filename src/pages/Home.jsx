4a43bbd60485bbbf3acef4dedfbb06a2cddb534b
zesponge <sumario82@gmail.com>
2023-11-22 13:49:00 -0500

    implemented island without lights or adjusting size

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
+import { Canvas } from '@react-three/fiber'
+import { Suspense } from 'react'
+import Loader from '../components/Loader'
+import Island from "../models/island.jsx"
 
 const Home = () => {
   return (
-    <div>
-      
-    </div>
+    <section className='w-full h-screen relative'>
+      {/* <div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>
+        Popup
+      </div> */}
+
+      <Canvas className='w-full h-screen bg-transparent' camera={{ near: 0.1, far: 1000  }}>
+        <Suspense fallback={<Loader/>}>
+          <directionalLight/>
+          <ambientLight/>
+          <Island/>
+        </Suspense>
+
+      </Canvas>
+
+
+    </section>
   )
 }
 
