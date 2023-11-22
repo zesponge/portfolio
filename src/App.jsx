09be81b667522f0b76394bdea7f1fc13062b8e41
zesponge <sumario82@gmail.com>
2023-11-21 22:26:12 -0500

    initial commit, finished navbar

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { Home, About, Projects, Contact } from './pages/index.js';
+import Navbar from './components/Navbar';
+
+const App = () => {
+  return (
+    <main className='bg-slate-300/20'>
+        <Router>
+            <Navbar></Navbar>
+            <Routes>
+                <Route path='/' element={<Home/>}/>
+                <Route path='/about' element={<About/>}/>\
+                <Route path='/projects' element={<Projects/>}/>
+                <Route path='/Contact' element={<Contact/>}/>
+            </Routes>
+        </Router>
+    </main>
+  )
+}
+
+export default App
