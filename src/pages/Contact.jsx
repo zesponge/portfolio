f0178d7574ebba40d93da5f8c0d42f102e7aa661
zesponge <sumario82@gmail.com>
2023-11-30 11:32:41 -0500

    update notes + change contacts pag

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -29,15 +29,15 @@ const Contact = () => {
         <h1 className='head-text'>Get in Touch</h1>
         <form onSubmit={handleSubmit} className='w-full flex flex-col gap-7 mt-14'>
           <label className='text-black-500 font-semibold'>Name
-            <input type="text" name="name" className='input' placeholder='name' required value={form.name} onChange={handleChange}
+            <input type="text" name="name" className='input' placeholder='Name' required value={form.name} onChange={handleChange}
               onFocus={handleFocus} onBlur={handleBlur}/>
           </label>
           <label className='text-black-500 font-semibold'>Email
-            <input type="email" name="email" className='input' placeholder='email' required value={form.email} onChange={handleChange}
+            <input type="email" name="email" className='input' placeholder='Email' required value={form.email} onChange={handleChange}
               onFocus={handleFocus} onBlur={handleBlur}/>
           </label>
           <label className='text-black-500 font-semibold'>Your Message
-            <textarea type="text"  rows={4} name="message" className='input' placeholder='message' required value={form.message} onChange={handleChange}
+            <textarea type="text"  rows={4} name="message" className='input' placeholder='Message' required value={form.message} onChange={handleChange}
               onFocus={handleFocus} onBlur={handleBlur}/>
           </label>
           <button type="submit" className='btn' onFocus={handleFocus} onBlur={handleBlur} disabled={isLoading}>
