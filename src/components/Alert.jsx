8a43f8a49d19f733d56d88da1b17481aac7f48e8
zesponge <sumario82@gmail.com>
2023-11-30 21:51:37 -0500

    alert

---
+++ src/components/Alert.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Alert = () => {
+    return (
+        <div className='absolute top-10 left-0 right-0 flex justify-center items-center'>
+            <div className=''>
+
+            </div>
+            
+        </div>
+    )
+}
+
+export default Alert;
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { useState, useRef } from 'react'
 import emailjs from '@emailjs/browser'
+import useAlert from '../hooks/useAlert'
+import Alert from '../components/Alert'
 
 const Contact = () => {
   const formRef = useRef(null)
   const [form, setForm] = useState({ name: '', email: "", message: ""})
   const [isLoading, setIsLoading] = useState(false)
+  const [alert, showAlert, hideAlert] = useAlert();
 
   const handleChange = (e) => {
     setForm({ ...form, [e.target.name]: e.target.value });
@@ -28,34 +31,31 @@ const Contact = () => {
       import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
     ).then(() => {
       setIsLoading(false);
-      //show success message
-      //hide alert
+      showAlert({show: true, text: "Message sent successfully", type: "success"});
       setForm({ name: '', email: "", message: ""})
     }).catch((error) => {
       setIsLoading(false);
       console.log(error)
-      //show error message
+      showAlert({show: true, text: "I didn't receive your message", type: "danger"});
     })
   }
 
   return (
     <section className='relative flex lg:flex-row flex-col max-container'>
+      {alert.show && <Alert {...alert}/>}
       <div className='flex-1 min-w-[50%] flex flex-col'>
         <h1 className='head-text'>Get in Touch</h1>
         <form onSubmit={handleSubmit} className='w-full flex flex-col gap-7 mt-14'>
           <label className='text-black-500 font-semibold'>Name
-            <input type="text" name="name" className='input' placeholder='Name' required value={form.name} onChange={handleChange}
-              onFocus={handleFocus} onBlur={handleBlur}/>
+            <input type="text" name="name" className='input' placeholder='Name' required value={form.name} onChange={handleChange}/>
           </label>
           <label className='text-black-500 font-semibold'>Email
-            <input type="email" name="email" className='input' placeholder='Email' required value={form.email} onChange={handleChange}
-              onFocus={handleFocus} onBlur={handleBlur}/>
+            <input type="email" name="email" className='input' placeholder='Email' required value={form.email} onChange={handleChange}/>
           </label>
           <label className='text-black-500 font-semibold'>Your Message
-            <textarea type="text"  rows={4} name="message" className='input' placeholder='Message' required value={form.message} onChange={handleChange}
-              onFocus={handleFocus} onBlur={handleBlur}/>
+            <textarea type="text"  rows={4} name="message" className='input' placeholder='Message' required value={form.message} onChange={handleChange}/>
           </label>
-          <button type="submit" className='btn' onFocus={handleFocus} onBlur={handleBlur} disabled={isLoading}>
+          <button type="submit" className='btn' disabled={isLoading}>
             {isLoading ? "Sending..." : "Send Message"}
           </button>
         </form>
