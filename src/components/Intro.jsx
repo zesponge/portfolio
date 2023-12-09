15cc9a0f8bce2fce5ef401b569a1fc5e6217c084
zesponge <sumario82@gmail.com>
2023-12-09 15:18:14 -0500

    start intro section

---
+++ src/components/Intro.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Intro = () => {
+  return (
+    <section className='h-[75vh] bg-cyan-200'>
+    </section>
+  );
+};
+
+export default Intro;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,6 +7,7 @@ import Sky from "../models/Sky.jsx"
 import Bird from "../models/Bird.jsx";
 import Plane from "../models/Plane.jsx"
 import HomeInfo from '../components/HomeInfo.jsx'
+import Intro from "../components/Intro.jsx"
 
 const Home = () => {
   const [isRotating, setIsRotating] = useState(false);
@@ -43,42 +44,45 @@ const Home = () => {
   const [islandScale, islandPosition, islandRotation] = adjustIslandForScreenSize();
   const [planeScale, planePosition] = adjustPlaneForScreenSize();
   return (
-    <section className='w-full h-screen relative'>
+    <div>
+      <Intro/>
+      <section className='w-full h-screen relative'>
 
-      <div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>
-        {currentStage && <HomeInfo currentStage={currentStage}/>}
-      </div>
+        <div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>
+          {currentStage && <HomeInfo currentStage={currentStage}/>}
+        </div>
 
-      <Canvas 
-      className={`w-full h-screen bg-transparent' ${isRotating ? 'cursor-grabbing' : 'cursor-grab'}`}
-      camera={{ near: 0.1, far: 1000  }}>
-        <Suspense fallback={<Loader/>}>
-          <directionalLight position={[1,1,1]} intensity={.5}/> //intensity 2 might be better
-          <ambientLight intensity={0.5}/>
-          <hemisphereLight skyColor="#b1e1ff" groundColor="#000000" intensity={1}/>
+        <Canvas 
+        className={`w-full h-screen bg-transparent' ${isRotating ? 'cursor-grabbing' : 'cursor-grab'}`}
+        camera={{ near: 0.1, far: 1000  }}>
+          <Suspense fallback={<Loader/>}>
+            <directionalLight position={[1,1,1]} intensity={.5}/> //intensity 2 might be better
+            <ambientLight intensity={0.5}/>
+            <hemisphereLight skyColor="#b1e1ff" groundColor="#000000" intensity={1}/>
 
-          <Bird/>
-          <Sky
-            isRotating={isRotating}/>
-          <Island 
-            position={islandPosition}
-            scale={islandScale}
-            rotation={islandRotation}
-            isRotating={isRotating}
-            setIsRotating={setIsRotating}
-            setCurrentStage={setCurrentStage}
-          />
-          <Plane
-            position={planePosition}
-            scale={planeScale}
-            isRotating={isRotating}
-            rotation={[0,20,0]}/>
-        </Suspense>
+            <Bird/>
+            <Sky
+              isRotating={isRotating}/>
+            <Island 
+              position={islandPosition}
+              scale={islandScale}
+              rotation={islandRotation}
+              isRotating={isRotating}
+              setIsRotating={setIsRotating}
+              setCurrentStage={setCurrentStage}
+            />
+            <Plane
+              position={planePosition}
+              scale={planeScale}
+              isRotating={isRotating}
+              rotation={[0,20,0]}/>
+          </Suspense>
 
-      </Canvas>
+        </Canvas>
 
 
-    </section>
+      </section>
+    </div>
   )
 }
 
